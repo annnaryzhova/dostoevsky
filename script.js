a86026fd1bd04ae45ce09b06c84ccde835586b02
Anna Ryzhova <s.ryjov@gmail.com>
2025-03-18 22:58:07 +0300

    Все работает

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+/**
+ * Сбрасываем все кнопки в положение "выкл."
+ */
 function resetButtons() {
     document.querySelector('nav').childNodes.forEach(button => {
-        button.classList && button.classList.remove('selected');
+        button.classList && button.classList.remove('selected-section');
     });
 }
 
+/**
+ * Скрываем все секции
+ */
 function hideAll() {
     document.querySelectorAll('section').forEach(page => {
         page.style.display = 'none';
@@ -11,28 +17,47 @@ function hideAll() {
     resetButtons();
 }
 
+/**
+ * Показывем секцию
+ * 
+ * @param {*} button  - элемент, кнопка соотв.секции
+ */
 function showSection(button) {
     hideAll();
     const section = document.getElementById(`section-${button.id}`);
-    button.classList.add('selected');
+    button.classList.add('selected-section');
     if (section) {
         section.style.display = 'block';
     }
 }
 
-function toggleBook(bookId) {
-    const book = document.getElementById(bookId);
+/**
+ * "Переключаем" книгу:
+ * 1. элемент с описанием и картинкой(ссылкой) показываем или прячем. 
+ * 2. Меняем стиль "палец вверх/вправо"
+ * 
+ * @param {*} bookTitle 
+ */
+function toggleBook(bookTitle) {
+    const book = document.getElementById(`book-${bookTitle.id}`);
     if (book.style.display !== 'none') {
         book.style.display = 'none';
+        bookTitle.parentElement.classList.remove('selected-book');
     } else {
         book.style.display = 'flex';
+        book.style.flexWrap = 'wrap-reverse';   // иначе свойство слетит
+        bookTitle.parentElement.classList.add('selected-book');
     }
 }
 
+// Кнопка переключения темы
 const themeToggle = document.getElementById('theme-toggle');
 const themes = ['gray', 'beige'];
 let currentThemeIndex = 0;
 
+/**
+ * Переключаем тему
+ */
 function toggleTheme() {
     document.body.classList.remove(`${themes[currentThemeIndex]}-theme`);
     currentThemeIndex = (currentThemeIndex + 1) % themes.length;
